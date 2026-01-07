47143dfe2e71dfd18cfb10539af6f4c8928a2609
Adriana-st <S00273868@atu.ie>
2026-01-07 10:18:34 +0000

    Added HouseholdRobot class

---
+++ Exam/HouseholdRobot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    public class HouseholdRobot: Robot
+    {
+        // Properties
+        private List<HouseholdSkill> Skills {  get; set; }
+
+        // Constructors
+
+        // Methods
+        public override string DescribeRobot()
+        {
+            string skills = "";
+            foreach (HouseholdSkill skill in Skills)
+            {
+                skills += $"{skill} \n";
+            }
+            return $"I am a household robot. \nI can help with chores around the house. \nHousehold Robot Skills: \n{skills} \n{DisplayBatteryInformation()}";
+        }
+    }
+}
