76b81dcfd4d492319f135876530dce0bc2a326bd
Adriana-st <S00273868@atu.ie>
2026-01-07 10:26:06 +0000

    Added class DeliveryRobot

---
+++ Exam/DeliveryRobot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    public class DeliveryRobot: Robot
+    {
+        public DeliveryMode ModeOfDelivery {  get; set; }
+        public double MaxLoadKG { get; set; }
+
+        public override string DescribeRobot()
+        {
+            return $"I am a delivery robot. \n\nI specialise in delivery by {ModeOfDelivery} \nThe maximum load I can carry is {MaxLoadKG} kg \n\n{DisplayBatteryInformation()}";
+        }
+    }
+}
